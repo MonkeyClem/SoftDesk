5958dd2f51f26b583da5a37a7d07300084dd8ebf
Clément | Igonogo <clement@igonogo.io>
2025-05-10 23:43:52 +0200

    Added the UserViewSet

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from django.contrib.auth import get_user_model
+from .serializer import UserSerializer
 # Create your views here.
+
+User = get_user_model()
+
+class UserViewSet: 
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
